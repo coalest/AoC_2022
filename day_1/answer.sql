4985f32b5ec79d116e2779ebcf80b7f7283b0891
Cory Streiff <cory@ragnarson.com>
2022-12-01 20:18:32 +0100

    Day 1

---
+++ day_1/answer.sql
@@ -0,0 +1,49 @@
+DROP TABLE IF EXISTS inputs;
+
+CREATE TABLE inputs (
+  id    SERIAL,
+  input INTEGER
+);
+
+\COPY inputs (input) FROM 'day_1/input.txt' WITH (FORMAT 'csv');
+
+-- Part 1
+WITH starts AS (
+    SELECT
+        id,
+        input,
+        CASE WHEN input IS NULL THEN 1 ELSE 0 END AS island_start
+    FROM inputs
+),
+islands AS (
+    SELECT
+        starts.input,
+        SUM(starts.island_start) OVER (ORDER BY starts.id ASC) AS island
+    FROM starts
+)
+SELECT SUM(COALESCE(islands.input, 0)) OVER (PARTITION BY islands.island) AS answer
+FROM islands
+ORDER BY answer DESC
+LIMIT 1;
+
+-- Part 2
+WITH starts AS (
+    SELECT
+        id,
+        input,
+        CASE WHEN input IS NULL THEN 1 ELSE 0 END AS island_start
+    FROM inputs
+),
+islands AS (
+    SELECT
+        starts.input,
+        SUM(starts.island_start) OVER (ORDER BY starts.id ASC) AS island
+    FROM starts
+)
+SELECT SUM(top_three.sum) answer
+FROM (SELECT
+    DISTINCT islands.island,
+    SUM(COALESCE(islands.input, 0)) OVER (PARTITION BY islands.island) AS sum
+    FROM islands
+    ORDER BY sum DESC
+    LIMIT 3) AS top_three;
