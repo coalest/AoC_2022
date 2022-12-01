67d39234c4d83de38c7304920cd0ebb4ea20707f
Cory Streiff <cory@ragnarson.com>
2022-12-01 20:53:49 +0100

    Fix linting

--- day_1/answer.sql
+++ day_1/answer.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS inputs;
 
 CREATE TABLE inputs (
-  id    SERIAL,
-  input INTEGER
+    id SERIAL,
+    input INTEGER
 );
 
 \COPY inputs (input) FROM 'day_1/input.txt' WITH (FORMAT 'csv');
@@ -15,12 +15,14 @@ WITH starts AS (
         CASE WHEN input IS NULL THEN 1 ELSE 0 END AS island_start
     FROM inputs
 ),
+
 islands AS (
     SELECT
         starts.input,
         SUM(starts.island_start) OVER (ORDER BY starts.id ASC) AS island
     FROM starts
 )
+
 SELECT SUM(COALESCE(islands.input, 0)) OVER (PARTITION BY islands.island) AS answer
 FROM islands
 ORDER BY answer DESC
@@ -34,16 +36,20 @@ WITH starts AS (
         CASE WHEN input IS NULL THEN 1 ELSE 0 END AS island_start
     FROM inputs
 ),
+
 islands AS (
     SELECT
         starts.input,
         SUM(starts.island_start) OVER (ORDER BY starts.id ASC) AS island
     FROM starts
 )
-SELECT SUM(top_three.sum) answer
-FROM (SELECT
-    DISTINCT islands.island,
-    SUM(COALESCE(islands.input, 0)) OVER (PARTITION BY islands.island) AS sum
+
+SELECT SUM(top_three.sum) AS answer
+FROM (SELECT DISTINCT
+    islands.island,
+    SUM(
+        COALESCE(islands.input, 0)
+    ) OVER (PARTITION BY islands.island) AS sum
     FROM islands
     ORDER BY sum DESC
     LIMIT 3) AS top_three;
