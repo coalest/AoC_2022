447285961d06dc2ac97038497f2930e90de41aeb
Cory Streiff <cory@ragnarson.com>
2022-12-06 14:01:21 +0100

    Refactor string concatenation into string_agg

--- day06/answer.sql
+++ day06/answer.sql
@@ -1,10 +1,8 @@
---begin;
+begin;
 
 create schema if not exists day06;
 
 set search_path to day06;
-
-drop table if exists raw_signal;
 
 create table raw_signal (
     id serial,
@@ -14,73 +12,62 @@ create table raw_signal (
 \COPY raw_signal (signal) FROM 'day06/input.txt' WITH (FORMAT 'csv');
 
 -- Part 1
-
 -- Separate input string into individual characters
-with chars as (
-    select
-        id,
-        unnest(string_to_array(signal, null)) as single_char
+with chars(chr) as (
+    select unnest(string_to_array(signal, null)) as chr
     from raw_signal
 ),
 
--- Group individual chars into 4 char long strings
-char_groups as (
+-- Add row numbers to keep order of chars
+with_row_numbers as (
     select
-        single_char ||
-        lead(single_char, 1) over () ||
-        lead(single_char, 2) over () ||
-        lead(single_char, 3) over ()
+        chr,
+        row_number() over () as id
+    from chars
+),
+
+-- Group individual chars into 4 char long substrings
+char_groups_1(four_chars, n) as (
+    select
+        string_agg(chr, null)
+        over (order by id range between 0 following and 3 following)
         as four_chars,
         row_number() over () + 3 as n
-    from chars
-)
+    from with_row_numbers
+),
 
 -- Find the first string that has 4 unique characters
-select n as part1_answer
-from char_groups, unnest(string_to_array(four_chars, null)) as chrs
-group by n
-having count(distinct chrs) = 4
-order by n
-limit 1;
+part_1 as (
+    select n as part1_answer
+    from char_groups_1, unnest(string_to_array(four_chars, null)) as chrs
+    group by n
+    having count(distinct chrs) = 4
+    order by n
+    limit 1
+),
 
 -- Part 2
 -- Same as part 1 but with 14 character long substrings
 
-with chars as (
+char_groups_2(fourteen_chars, n) as (
     select
-        id,
-        unnest(string_to_array(signal, null)) as single_char
-    from raw_signal
-),
-
-char_groups as (
-    select
-        single_char ||
-        lead(single_char, 1) over () ||
-        lead(single_char, 2) over () ||
-        lead(single_char, 3) over () ||
-        lead(single_char, 4) over () ||
-        lead(single_char, 5) over () ||
-        lead(single_char, 6) over () ||
-        lead(single_char, 7) over () ||
-        lead(single_char, 8) over () ||
-        lead(single_char, 9) over () ||
-        lead(single_char, 10) over () ||
-        lead(single_char, 11) over () ||
-        lead(single_char, 12) over () ||
-        lead(single_char, 11) over () ||
-        lead(single_char, 12) over () ||
-        lead(single_char, 13) over ()
+        string_agg(chr, null)
+        over (order by id range between 0 following and 13 following)
         as fourteen_chars,
         row_number() over () + 13 as n
-    from chars
+    from with_row_numbers
+),
+
+part_2 as (
+    select n as part2_answer
+    from char_groups_2, unnest(string_to_array(fourteen_chars, null)) as chrs
+    group by n
+    having count(distinct chrs) = 14
+    order by n
+    limit 1
 )
 
-select n as part2_answer
-from char_groups, unnest(string_to_array(fourteen_chars, null)) as chrs
-group by n
-having count(distinct chrs) = 14
-order by n
-limit 1;
+select *
+from part_1, part_2;
 
---rollback;
+rollback;
